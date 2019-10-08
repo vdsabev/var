7b9f5d08384376c7e672b4555000be87c1a9ee2f
7opf <sevi.skill@gmail.com>
2019-10-08 20:07:07 +0100

    use path.resolve instead of path.join (#6)
    Allows for use of an absolute URL in ENV_VAR_CONFIG_FILE

--- paths.js
+++ paths.js
@@ -2,4 +2,4 @@ var path = require('path');
 
 exports.CONFIG = process.env.ENV_VAR_CONFIG_FILE || 'env.json';
 
-exports.get = (fileName) => path.join(process.cwd(), fileName);
+exports.get = (fileName) => path.resolve(process.cwd(), fileName);
